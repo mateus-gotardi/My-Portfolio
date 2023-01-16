9b9d9ef44858334bd5bded5648a8916da337ee7a
mateus gotardi <mateus.gotardi@gmail.com>
2023-01-16 14:32:37 -0300

    add context variables for language change

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,30 @@
 import '../styles/globals.css'
 import '../styles/animation.css'
 import App from 'next/app'
+import AppContext from '../AppContext'
+import { useState } from "react";
 
-class MyApp extends App {
-  render() {
-    const { Component, pageProps } = this.props
-    return <Component {...pageProps} />
-  }
+
+function MyApp({ Component, pageProps }) {
+  const [english, setEnglish] = useState(false);
+  const englishTexts = {}
+  const portugueseTexts = {}
+  return (
+    <AppContext.Provider
+      value={{
+        state: {
+          english: english,
+        },
+        languages: {
+          englishTexts,
+          portugueseTexts
+        },
+        setEnglish: setEnglish,
+      }}
+    >
+      <Component {...pageProps} />
+    </AppContext.Provider>
+  );
 }
 
 export default MyApp
